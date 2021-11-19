1d71de1b7f8ff1800251723bc1ad7a438562908b
Daniel Svirida <75013679+Danzzilla@users.noreply.github.com>
2021-11-19 15:03:08 -0800

    Add files via upload

---
+++ cars_dataset/script/data.js
@@ -0,0 +1,731 @@
+/*
+Author: Daniel Svirida
+filename: data.js
+date: 11/17/21
+
+This file contains the dataset of many different cars
+stored as objects
+*/
+
+let cars = [
+  {
+    "Dimensions": {
+      "Height": 140,
+      "Length": 143,
+      "Width": 202
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Audi 3.2L 6 cylinder 250hp 236ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 250,
+        "Torque": 236
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 25
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2009 Audi A3 3.2",
+      "Make": "Audi",
+      "Model Year": "2009 Audi A3",
+      "Year": 2009
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 201,
+      "Length": 221,
+      "Width": 191
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Audi 3.6L 6 cylinder 280hp 266 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 280,
+        "Torque": 266
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 14,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 20
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2009 Audi Q7 3.6",
+      "Make": "Audi",
+      "Model Year": "2009 Audi Q7",
+      "Year": 2009
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 147,
+      "Length": 96,
+      "Width": 34
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Audi 2.0L 4 cylinder 211 hp 258 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 211,
+        "Torque": 258
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 22,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 30
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2009 Audi A4 Sedan 2.0 T Quattro",
+      "Make": "Audi",
+      "Model Year": "2009 Audi A4 Sedan",
+      "Year": 2009
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 147,
+      "Length": 96,
+      "Width": 34
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Audi 2.0L 4 cylinder 211 hp 258 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 211,
+        "Torque": 258
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 21,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 27
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2009 Audi A4 Sedan 2.0 T Quattro AT",
+      "Make": "Audi",
+      "Model Year": "2009 Audi A4 Sedan",
+      "Year": 2009
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 172,
+      "Length": 63,
+      "Width": 87
+    },
+    "Engine Information": {
+      "Driveline": "Front-wheel drive",
+      "Engine Type": "Acura 3.5L 6 Cylinder 280 hp 254 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 280,
+        "Torque": 254
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 20,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 29
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2012 Acura TL",
+      "Make": "Acura",
+      "Model Year": "2012 Acura TL",
+      "Year": 2012
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 172,
+      "Length": 63,
+      "Width": 87
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Acura 3.7L 6 Cylinder 305 hp 273 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 305,
+        "Torque": 273
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 26
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2012 Acura TL SH-AWD",
+      "Make": "Acura",
+      "Model Year": "2012 Acura TL",
+      "Year": 2012
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 226,
+      "Length": 243,
+      "Width": 201
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "BMW 4.4L 8 cylinder 555hp 500 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 555,
+        "Torque": 500
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 12,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 BMW X5 M SUV",
+      "Make": "BMW",
+      "Model Year": "2010 BMW X5",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 226,
+      "Length": 243,
+      "Width": 201
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "BMW 4.4L 8 cylinder 555hp 500 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 555,
+        "Torque": 500
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 12,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2011 BMW X5 M SUV",
+      "Make": "BMW",
+      "Model Year": "2011 BMW X5",
+      "Year": 2011
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 148,
+      "Length": 12,
+      "Width": 191
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "BMW 4.4L 8 cylinder 555hp 500 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 555,
+        "Torque": 500
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 12,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 BMW X6 M",
+      "Make": "BMW",
+      "Model Year": "2010 BMW X6",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 148,
+      "Length": 12,
+      "Width": 191
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "BMW 4.4L 8 cylinder 555hp 500 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic Select Shift",
+      "Engine Statistics": {
+        "Horsepower": 555,
+        "Torque": 500
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 12,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2011 BMW X6 M",
+      "Make": "BMW",
+      "Model Year": "2011 BMW X6",
+      "Year": 2011
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 112,
+      "Length": 22,
+      "Width": 181
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Chevrolet 2.9L 4 Cylinder 185 hp 190 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 5,
+      "Transmission": "5 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 185,
+        "Torque": 190
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 25
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2012 Chevrolet Colorado Work Truck",
+      "Make": "Chevrolet",
+      "Model Year": "2012 Chevrolet Colorado",
+      "Year": 2012
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 112,
+      "Length": 140,
+      "Width": 181
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Chevrolet 2.9L 4 Cylinder 185 hp 190 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 5,
+      "Transmission": "5 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 185,
+        "Torque": 190
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 25
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2012 Chevrolet Colorado Extended Cab Work Truck",
+      "Make": "Chevrolet",
+      "Model Year": "2012 Chevrolet Colorado",
+      "Year": 2012
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 77,
+      "Length": 102,
+      "Width": 237
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Chevrolet 4.3L 6 Cylinder 195 hp 260 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 4,
+      "Transmission": "4 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 195,
+        "Torque": 260
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 15,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 20
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 Chevrolet Silverado 1500 Work Truck",
+      "Make": "Chevrolet",
+      "Model Year": "2010 Chevrolet Silverado 1500",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 82,
+      "Length": 215,
+      "Width": 240
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Chevrolet 4.8L 8 Cylinder 302 hp 305 ft-lbs FFV",
+      "Hybrid": true,
+      "Number of Forward Gears": 4,
+      "Transmission": "4 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 302,
+        "Torque": 305
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 14,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 19
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 Chevrolet Silverado 1500 Crew Cab Work Truck",
+      "Make": "Chevrolet",
+      "Model Year": "2010 Chevrolet Silverado 1500",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 43,
+      "Length": 52,
+      "Width": 52
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Nissan 3.7L 6 Cylinder 350hp 276 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 350,
+        "Torque": 276
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 26
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2011 Nissan 370Z NISMO",
+      "Make": "Nissan",
+      "Model Year": "2011 Nissan 370Z Coupe",
+      "Year": 2011
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 35,
+      "Length": 75,
+      "Width": 52
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Nissan 3.7L 6 Cylinder 350hp 276 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 350,
+        "Torque": 276
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 26
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2010 Nissan 370Z NISMO",
+      "Make": "Nissan",
+      "Model Year": "2010 Nissan 370Z Coupe",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 35,
+      "Length": 75,
+      "Width": 52
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Nissan 3.7L 6 Cylinder 350hp 276 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 350,
+        "Torque": 276
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 18,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 26
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2009 Nissan 370Z NISMO",
+      "Make": "Nissan",
+      "Model Year": "2009 Nissan 370Z Coupe",
+      "Year": 2009
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 167,
+      "Length": 168,
+      "Width": 247
+    },
+    "Engine Information": {
+      "Driveline": "Front-wheel drive",
+      "Engine Type": "Volvo 2.5L 5 Cylinder 227 hp 236 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 227,
+        "Torque": 236
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 21,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 29
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2011 Volvo C30 T5",
+      "Make": "Volvo",
+      "Model Year": "2011 Volvo C30",
+      "Year": 2011
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 167,
+      "Length": 155,
+      "Width": 247
+    },
+    "Engine Information": {
+      "Driveline": "Front-wheel drive",
+      "Engine Type": "Volvo 2.5L 5 Cylinder 227 hp 236 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 227,
+        "Torque": 236
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 21,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 29
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2010 Volvo C30 T5",
+      "Make": "Volvo",
+      "Model Year": "2010 Volvo C30",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 193,
+      "Length": 34,
+      "Width": 107
+    },
+    "Engine Information": {
+      "Driveline": "Rear-wheel drive",
+      "Engine Type": "Bentley 6.8L 8 cylinder 530hp 774 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 530,
+        "Torque": 774
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 9,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 15
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 Bentley Brooklands",
+      "Make": "Bentley",
+      "Model Year": "2010 Bentley Brooklands",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 185,
+      "Length": 170,
+      "Width": 184
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Bentley 6.0L 12 Cylinder 600 hp 553 ft-lbs FFV Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 600,
+        "Torque": 553
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 10,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2011 Bentley Continental Flying Spur Speed",
+      "Make": "Bentley",
+      "Model Year": "2011 Bentley Continental Flying Spur",
+      "Year": 2011
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 185,
+      "Length": 170,
+      "Width": 184
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Bentley 6.0L 12 cylinder 600hp 553 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 600,
+        "Torque": 553
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 10,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 Bentley Continental Flying Spur Speed",
+      "Make": "Bentley",
+      "Model Year": "2010 Bentley Continental Flying Spur",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 195,
+      "Length": 170,
+      "Width": 184
+    },
+    "Engine Information": {
+      "Driveline": "All-wheel drive",
+      "Engine Type": "Bentley 6.0L 12 cylinder 552 hp 479 ft-lbs Turbo",
+      "Hybrid": true,
+      "Number of Forward Gears": 6,
+      "Transmission": "6 Speed Automatic",
+      "Engine Statistics": {
+        "Horsepower": 552,
+        "Torque": 479
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 10,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 17
+    },
+    "Identification": {
+      "Classification": "Automatic transmission",
+      "ID": "2010 Bentley Continental Flying Spur",
+      "Make": "Bentley",
+      "Model Year": "2010 Bentley Continental Flying Spur",
+      "Year": 2010
+    }
+  },
+  {
+    "Dimensions": {
+      "Height": 152,
+      "Length": 240,
+      "Width": 57
+    },
+    "Engine Information": {
+      "Driveline": "Front-wheel drive",
+      "Engine Type": "Honda 2.4L 4 cylinder 190 hp 162 ft-lbs",
+      "Hybrid": true,
+      "Number of Forward Gears": 5,
+      "Transmission": "5 Speed Manual",
+      "Engine Statistics": {
+        "Horsepower": 190,
+        "Torque": 162
+      }
+    },
+    "Fuel Information": {
+      "City mpg": 22,
+      "Fuel Type": "Gasoline",
+      "Highway mpg": 31
+    },
+    "Identification": {
+      "Classification": "Manual transmission",
+      "ID": "2010 Honda Accord EX Coup",
+      "Make": "Honda",
+      "Model Year": "2010 Honda Accord Coupe",
+      "Year": 2010
+    }
+  }
+]
---
+++ cars_dataset/script/dataManager.js
@@ -0,0 +1,126 @@
+/*
+Author: Daniel Svirida
+filename: dataManager.js
+date: 11/17/21
+
+This file manages the data in the data.js file and
+fills in fields throughout the page with car specifications
+and facts
+*/
+
+"use strict";
+//access to buttons
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+//access to info fields
+const field1 = document.querySelector("#field1");
+const field2 = document.querySelector("#field2");
+const field3 = document.querySelector("#field3");
+const field4 = document.querySelector("#field4");
+const field5 = document.querySelector("#field5");
+const field6 = document.querySelector("#field6");
+const field7 = document.querySelector("#field7");
+
+//access to interesting facts fields
+const data_num = document.querySelector("#data_num");
+const fact1 = document.querySelector("#fact1");
+const fact2 = document.querySelector("#fact2");
+const fact3 = document.querySelector("#fact3");
+const fact4 = document.querySelector("#fact4");
+
+//index of current object
+let index = 0;
+
+//fills in all the fields
+start();
+
+//previous button
+previous.onclick = function(){
+    if(index > 0){
+        index--;
+        display();
+    } else {
+        index = cars.length - 1;
+        display();
+    }
+}
+
+//next button
+next.onclick = function(){
+    if(index < cars.length - 1){
+        index++;
+        display();
+    } else {
+        index = 0;
+        display();
+    }
+}
+
+//displays record in array at current index
+function display(){
+    field1.innerHTML = cars[index]["Identification"]["ID"];
+    field2.innerHTML = `Driveline: ${cars[index]["Engine Information"]["Driveline"]}`;
+    field3.innerHTML = `Engine: ${cars[index]["Engine Information"]["Engine Type"]}`;
+    field4.innerHTML = `Transmission: ${cars[index]["Engine Information"]["Transmission"]}`;
+    field5.innerHTML = `Fuel Type: ${cars[index]["Fuel Information"]["Fuel Type"]}`;
+    field6.innerHTML = `City MPG: ${cars[index]["Fuel Information"]["City mpg"]}`;
+    field7.innerHTML = `Highway MPG: ${cars[index]["Fuel Information"]["Highway mpg"]}`;
+}
+
+//starts off the page with all the fields filled in
+function start(){
+    display();
+
+    data_num.innerHTML = cars.length;
+    fact1.innerHTML = hpAverage().toFixed(2);
+    fact2.innerHTML = BMWCount();
+    let slowest_car = slowestCar();
+    fact3.innerHTML = slowest_car["Identification"]["ID"];
+    let drift_car = driftPotential();
+    fact4.innerHTML = drift_car["Identification"]["ID"];
+}
+
+//finds the average horsepower of cars in database
+function hpAverage(){
+    let sum = 0;
+    for(let i = 0; i < cars.length - 1; i++){
+        sum += cars[i]["Engine Information"]["Engine Statistics"]["Horsepower"];
+    }
+    let hpAverage = sum / cars.length;
+
+    return hpAverage;
+}
+
+//finds number of mercedes in database
+function BMWCount(){
+    let count = 0;
+    for(let i = 0; i < cars.length - 1; i++){
+        if(cars[i]["Identification"]["Make"] === "BMW") count++;
+    }
+    return count;
+}
+
+//finds slowest car in database
+function slowestCar(){
+    let stored_car = cars[0];
+    for(let i = 0; i < cars.length - 1; i++){
+        if(cars[i]["Engine Information"]["Engine Statistics"]["Horsepower"] < stored_car["Engine Information"]["Engine Statistics"]["Horsepower"]){
+            stored_car = cars[i];
+        }
+    }
+    return stored_car;
+}
+
+//finds car with highest drift potential
+function driftPotential(){
+    let stored_car = cars[0]
+    for(let i = 0; i < cars.length - 1; i++){
+        if((cars[i]["Engine Information"]["Driveline"] === "Rear-wheel drive") &&
+        ((cars[i]["Engine Information"]["Transmission"] === "6 Speed Manual") || (cars[i]["Engine Information"]["Transmission"] === "5 Speed Manual"))&&
+        (cars[i]["Engine Information"]["Engine Statistics"]["Torque"] > stored_car["Engine Information"]["Engine Statistics"]["Torque"])){
+            stored_car = cars[i];
+        }
+    }
+    return stored_car;
+}
